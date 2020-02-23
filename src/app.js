e1929612a4dbd3b3c63c675e2e885339535d3024
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2020-02-22 18:48:25 -0600

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
+
 
 const app = express()
 
@@ -15,9 +17,39 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+         .then(articles => {
+            res.json(articles.map(article => ({
+                         id: article.id,
+                         title: article.title,
+                         style: article.style,
+                         content: article.content,
+                         date_published: new Date(article.date_published),
+                      })))
+         })
+         .catch(next)
+     })
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+       ArticlesService.getById(knexInstance, req.params.article_id)
+         .then(article => {
+            res.json({
+                         id: article.id,
+                         title: article.title,
+                         style: article.style,
+                        content: article.content,
+                         date_published: new Date(article.date_published),
+                       })
+        })
+         .catch(next)
+      })
 
 app.use(function errorHandler(error, req, res, next) {
     let response
