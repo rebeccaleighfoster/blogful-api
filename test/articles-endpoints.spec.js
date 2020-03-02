e4c5a7b2446725df5e17a8dcba4b9e77f71d6d1a
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2020-03-02 13:44:42 -0600

    checkpoint 19

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,6 +1,8 @@
 const knex = require('knex')
 const app = require('../src/app')
 const { makeArticlesArray } = require('./articles.fixtures')
+const { makeUsersArray } = require('./users.fixtures')
+
 
 describe('Articles Endpoints', function() {
   let db
@@ -10,7 +12,7 @@ describe('Articles Endpoints', function() {
       client: 'pg',
       connection: {
         host     : 'localhost',
-        port      : '5432',
+        port     : '5432',
         user     : 'postgres', 
         database : 'blogful',
         charset  : 'utf8',
@@ -22,9 +24,9 @@ describe('Articles Endpoints', function() {
 
   after('disconnect from db', () => db.destroy())
 
-  before('clean the table', () => db('blogful_articles').truncate())
+  before('clean the table', () => db.raw('TRUNCATE blogful_articles, blogful_users, blogful_comments RESTART IDENTITY CASCADE'))
 
-  afterEach('cleanup',() => db('blogful_articles').truncate())
+  afterEach('cleanup',() => db.raw('TRUNCATE blogful_articles, blogful_users, blogful_comments RESTART IDENTITY CASCADE'))
 
   describe(`GET /api/articles`, () => {
     context(`Given no articles`, () => {
@@ -36,12 +38,18 @@ describe('Articles Endpoints', function() {
     })
 
     context('Given there are articles in the database', () => {
-      const testArticles = makeArticlesArray()
+      const testUsers = makeUsersArray();
+      const testArticles = makeArticlesArray();
 
       beforeEach('insert articles', () => {
         return db
-          .into('blogful_articles')
-          .insert(testArticles)
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
       })
 
       it('responds with 200 and all of the articles', () => {
@@ -52,12 +60,18 @@ describe('Articles Endpoints', function() {
     })
 
     context(`Given an XSS attack article`, () => {
+      const testUsers = makeUsersArray();
       const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
 
       beforeEach('insert malicious article', () => {
         return db
-          .into('blogful_articles')
-          .insert([ maliciousArticle ])
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert([ maliciousArticle ])
+          })
       })
 
       it('removes XSS attack content', () => {
@@ -83,12 +97,18 @@ describe('Articles Endpoints', function() {
     })
 
     context('Given there are articles in the database', () => {
+      const testUsers = makeUsersArray();
       const testArticles = makeArticlesArray()
 
       beforeEach('insert articles', () => {
         return db
-          .into('blogful_articles')
-          .insert(testArticles)
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
       })
 
       it('responds with 200 and the specified article', () => {
@@ -101,12 +121,18 @@ describe('Articles Endpoints', function() {
     })
 
     context(`Given an XSS attack article`, () => {
+      const testUsers = makeUsersArray();
       const { maliciousArticle, expectedArticle } = makeMaliciousArticle()
 
       beforeEach('insert malicious article', () => {
         return db
-          .into('blogful_articles')
-          .insert([ maliciousArticle ])
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert([ maliciousArticle ])
+          })
       })
 
       it('removes XSS attack content', () => {
@@ -122,6 +148,13 @@ describe('Articles Endpoints', function() {
   })
 
   describe(`POST /api/articles`, () => {
+    const testUsers = makeUsersArray();
+    beforeEach('insert malicious article', () => {
+      return db
+        .into('blogful_users')
+        .insert(testUsers)
+    })
+
     it(`creates an article, responding with 201 and the new article`, () => {
       const newArticle = {
         title: 'Test new article',
@@ -138,8 +171,8 @@ describe('Articles Endpoints', function() {
           expect(res.body.content).to.eql(newArticle.content)
           expect(res.body).to.have.property('id')
           expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
-          const expected = new Date().toLocaleString()
-          const actual = new Date(res.body.date_published).toLocaleString()
+          const expected = new Intl.DateTimeFormat('en-US').format(new Date())
+          const actual = new Intl.DateTimeFormat('en-US').format(new Date(res.body.date_published))
           expect(actual).to.eql(expected)
         })
         .then(res =>
@@ -194,12 +227,18 @@ describe('Articles Endpoints', function() {
     })
 
     context('Given there are articles in the database', () => {
+      const testUsers = makeUsersArray();
       const testArticles = makeArticlesArray()
 
       beforeEach('insert articles', () => {
         return db
-          .into('blogful_articles')
-          .insert(testArticles)
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
       })
 
       it('responds with 204 and removes the article', () => {
@@ -228,12 +267,18 @@ describe('Articles Endpoints', function() {
     })
 
     context('Given there are articles in the database', () => {
+      const testUsers = makeUsersArray();
       const testArticles = makeArticlesArray()
 
       beforeEach('insert articles', () => {
         return db
-          .into('blogful_articles')
-          .insert(testArticles)
+          .into('blogful_users')
+          .insert(testUsers)
+          .then(() => {
+            return db
+              .into('blogful_articles')
+              .insert(testArticles)
+          })
       })
 
       it('responds with 204 and updates the article', () => {
@@ -265,7 +310,7 @@ describe('Articles Endpoints', function() {
           .send({ irrelevantField: 'foo' })
           .expect(400, {
             error: {
-              message: `Request body must content either 'title', 'style' or 'content'`
+              message: `Request body must contain either 'title', 'style' or 'content'`
             }
           })
       })
